13f4bf851b279abcbb8a873e99049f4896dffd29
ktndrnl <33733121+ktndrnl@users.noreply.github.com>
2019-07-25 21:30:30 -0500

    E09

---
+++ Procedural Caves/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private Rigidbody rigidbody;
+	private Vector3 velocity;
+
+	private void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * 10;
+	}
+
+	private void FixedUpdate()
+	{
+		rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+}
---
+++ Procedural Caves/Assets/Player2D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2D : MonoBehaviour
+{
+	private Rigidbody2D rigidbody;
+	private Vector2 velocity;
+
+	private void Start()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * 10;
+	}
+
+	private void FixedUpdate()
+	{
+		rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+}
